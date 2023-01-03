b2cec4fc7f4d4cf9acb96aa9f85ddb5be8dffdb4
Damian Alanis <damian.alanis98@gmail.com>
2023-01-03 02:12:43 -0600

    Add additional dependencies & events section

---
+++ pages/events/index.tsx
@@ -0,0 +1,10 @@
+import { Heading, Container } from '@chakra-ui/react';
+
+
+const Events: React.FC = () => (
+    <Container marginTop='5'>
+        <Heading as='h3' size='2xl' textAlign='center'>All events</Heading>
+    </Container>
+);
+
+export default Events;
